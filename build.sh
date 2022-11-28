aea7d1f5e82e1419ca4046f8b4c2fe46f2ba95d7
JoelByers <joel.byers@outlook.com>
2022-11-28 12:27:55 -0600

    Add ParallelSolver

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 g++ src/SerialSolver.cpp src/SudokuPuzzle.cpp src/IO.cpp -fopenmp -o sudokuSerial.out
+g++ src/ParallelSolver.cpp src/SudokuPuzzle.cpp src/IO.cpp -fopenmp -o sudokuParallel.out
 g++ src/SudokuPuzzleGen.cpp src/SudokuPuzzle.cpp -o gen.out
